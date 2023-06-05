0eafe42682d5835c0f248f9f8be79447ec90e980
JRHemmen <bobertbarker101@gmail.com>
2023-06-04 22:00:30 -0400

    sensor loopin

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,104 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_event.h"
-#include "esp_wifi.h"
-#include "nvs_flash.h"
 #include "esp_log.h"
+#include "freertos/semphr.h"
 
-// Hardcoded Wifi and MQTT Settings
-#define WIFI_SSID "SigmaNet"
-#define WIFI_PASSWORD "YourPassword"
+
+#include "WiFi.h"
+#include <PubSubClient.h>
+
+#include <Ezo_i2c.h>
+#include <Wire.h>
+#include "nvs_flash.h"
+
+// Global constants, vars, and initializations
+#define WIFI_SSID "MyNetwork"
+#define WIFI_PASSWORD "MyPassword"
 
 #define MQTT_SERVER "mqtt://your-mqtt-server"
 #define MQTT_PORT 1883
 
+SemaphoreHandle_t i2c_semaphore = NULL;
 
-void my_setup()
+Ezo_board PH = Ezo_board(99, "PH");
+//Ezo_board EC = Ezo_board(100, "EC");
+
+
+struct Device {
+  Ezo_board board; // How to be agnostic of ezo_board library?
+  int pollingRate; // in ms
+  const char* mqttTopic; // topic to publish to
+};
+
+Device device_list[] = {
+  {Ezo_board(99, "PH"), 1000, "hydroponics/ph"},
+  //{Ezo_board(100, "EC"), 2000, "hydroponics/ec"},
+  // add more devices here...
+};
+
+void wifi_setup()
 {
-  // Setup Wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  // Create Sensor Objects
+  ESP_LOGI("WIFI", "Attempting to connect to %s", WIFI_SSID);
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    vTaskDelay(500);
+    ESP_LOGI("WIFI", "Connecting to %s", WIFI_SSID);
+  }
 
-  // Create new RTOS tasks for each sensor
+  ESP_LOGI("WIFI", "Connected to %s", WIFI_SSID);
+  ESP_LOGI("WIFI", "IP Address: %s", WiFi.localIP().toString().c_str());
 
-  // Setup MQTT
-
+  //TODO: failure and retry handling
 
 }
 
+void mqtt_setup()
+{
+  mqtt_client.setServer(MQTT_SERVER, MQTT_PORT);
 
+  //TODO: failure and retry handling
+}
+
+void sensor_loop(void* parameter)
+{
+  Device* device = (Device*) parameter;
+  char result[30];
+
+  while(1) {
+    xSemaphoreTake(i2c_semaphore, portMAX_DELAY);
+    device->board.send_read_cmd();
+    xSemaphoreGive(i2c_semaphore);
+
+    device->board.receive_read_cmd();
+    float reading = device->board.get_last_received_reading();
+    sprintf(result, "%.2f", reading); // convert float to string
+
+    mqtt_client.publish(device->mqttTopic, result);
+    ESP_LOGI("SENSOR", "Published reading: %s = %s", device->mqttTopic, result);
+
+    vTaskDelay(device->pollingRate / portTICK_PERIOD_MS);
+  }
+}
+
+
+
+
+void my_setup()
+{
+  wifi_setup();
+
+  mqtt_setup();
+
+  // Create new RTOS tasks for each sensor
+  for (int i = 0; i < sizeof(device_list); i++)
+  {
+    //xTaskCreate
+  }
+}
 
 extern "C" void app_main(void)
 {
@@ -37,8 +109,7 @@ extern "C" void app_main(void)
 
   while(1)
   {
-    sensor_loop();
+
   }
   
 }
-
